4f6ddd9406f97fb398f619c12334ad405e1309f9
imac <imac@iMac-admin.local>
2019-11-04 12:33:34 +0300

    frontend finished

---
+++ resources/js/components/AeroComponent.vue
@@ -0,0 +1,146 @@
+<template>
+    <div class="aero-component row">
+        
+
+        <div class="stantions col-md-4">
+            <div class="input filter w-100">
+                <input type="text" class="w-100 input-lg shadow form-control mb-3" name="filter-name" v-model="filtertext">
+            </div>
+            <div class="stantions-list shadow rounded">
+                <ul class="list-unstyled p-2 "
+                    >
+                    <li class="preloader btn btn-light btn-lg border-secondary w-100">  </li>
+                    <li class="btn btn-lg btn-light border-light  mb-1 w-100"
+                        v-for="st in stantionList"
+                        v-bind:disabled="ajaxBlock"
+                        @click="selectStantion(st)"
+                        v-bind:class="{ active : st.id==selectedStantion.id, disabled: ajaxBlock, loader:stloading }">
+                        {{ st.name }}
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-8">
+            <img class="img-fluid img-thumbnail"
+                 v-bind:src="pathToImg"
+                 v-bind:alt="selectedStantion.name"
+                 v-bind:class="{ 'd-none':pathToImg=='' }">
+
+            <div class="img-loader" 
+                 v-bind:class="{ 'd-none':pathToImg!=''&&!ajaxBlock }">                
+                 <img src="/loaders/rings.svg" width="100px">
+            </div>
+        </div>
+
+    </div>    
+</template>
+
+<script>
+    export default {
+        data:()=>({
+            /**
+             * Доступные для выбора станции (по которым есть данные)
+             */
+            stantions:[],
+            /**
+             * выбранная станция, по которой шлем запрос
+             * @type {Array}
+             */
+            selectedStantion:{},
+            /**
+             * путь к картинке
+             * @type {String}
+             */
+            pathToImg:'',
+            /**
+             * составные части URl для запроса данных
+             * @type {Object}
+             */
+            urls:{
+                stantions:'/aero/',
+                draw:'/aero/'
+            },
+            // errors
+            errors:[],
+            // фильтруем станции по совпадению
+            filtertext:'',
+            //индикатор загрузки станций
+            stloading:false,
+            // блокируем все аякс запросы
+            ajaxBlock:false,
+        }),
+
+        mounted() {
+            console.log('Component mounted.');
+            this.loadStantions();
+        },
+
+        computed:{
+            stantionList(){
+                var filtered = this.stantions;
+                var _vue = this;
+                // если введена часть строки поиска, то фильтруем
+                if (_vue.filtertext!='') {
+                    filtered = [];
+                    for( var item in this.stantions ){
+                        if ( String(this.stantions[item].name).indexOf(_vue.filtertext)!=-1 || String(this.stantions[item].id).indexOf(_vue.filtertext)!=-1 ){
+                            filtered.push( this.stantions[item] );                            
+                        }
+                    }
+                }
+                return filtered;
+            }
+        },
+
+        methods:{
+
+            /**
+             * select needed stantions
+             * @param  {[type]} st [description]
+             * @return {[type]}    [description]
+             */
+            selectStantion(st){
+                this.selectedStantion = st;
+                this.pathToImg = '';
+                // блокируем все вызовы и ставим прелоадер
+                this.ajaxBlock=true;
+                axios.get(this.urls.draw+this.selectedStantion.id)
+                    .then( response =>{
+                        this.pathToImg = response.data.path;
+                        // разблокируем все
+                        this.ajaxBlock=false;
+                    } )
+                    .catch(e=>{
+                        this.errors.push(e);
+                        // разблокируем все
+                        this.ajaxBlock=false;
+                    });
+            },
+
+            /**
+             * загружаем список доступных станций
+             * @return {[type]} [description]
+             */
+            loadStantions(){
+                // блокируем все вызовы и ставим прелоадер
+                this.ajaxBlock=true;
+                this.stloading=true;
+                // делаем запрос станций
+                axios.get(this.urls.stantions)
+                    .then( response =>{
+                        this.stantions = response.data.stantions;
+                        // разблокируем все
+                        this.ajaxBlock=false;
+                        this.stloading=false;
+                    } )
+                    .catch(e=>{
+                        this.errors.push(e);
+                        // разблокируем все
+                        this.ajaxBlock=false;
+                        this.stloading=false;
+                    });
+            }
+        }
+    }
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,41 +5,38 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+ 
+        <link rel="stylesheet" href="/css/app.css">
+        
 
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <div class="wrapper">
+            <div class="header text-center">
+                <span class="logo"></span>
+                <h1>Adiagramm</h1>
+                <p>Плагин для Laravel для рисования аэрологических диаграмм. ©F2re</p>
+            </div>
+        
+        
+            <div class="main flex-center position-ref full-height" id="app">
+                
+                <div class="content rounded bg-faded bg-white p-5">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                    <aero-component></aero-component>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+    
+            </div>
+
+        
+
+            <div class="footer">
+                <script src="/js/app.js"></script>
             </div>
         </div>
     </body>
